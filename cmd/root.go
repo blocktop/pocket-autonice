f4ce21e505be694cd9d595de747db4a1ce9b6bbd
strobus <22136798+strobus@users.noreply.github.com>
2022-05-01 13:04:37 -0400

    eliminate client command

--- cmd/root.go
+++ cmd/root.go
@@ -1,8 +1,14 @@
 package cmd
 
 import (
+	"context"
+	"github.com/blocktop/pocket-autonice/client"
 	"github.com/blocktop/pocket-autonice/config"
+	"github.com/blocktop/pocket-autonice/prometheusPoller"
+	"github.com/spf13/viper"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/spf13/cobra"
 )
@@ -11,29 +17,34 @@ var cfgFile string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "autonice",
-	Short: "Boost pocket and blockchains when they are serving relays.",
-	Long: `Pocket autonice uses the Linux operating system's "nice" feature
-to boost the CPU priority of the pocket process and the blockchain
-process when they are serving relays.
+	Use:   "client",
+	Short: "Runs the autonice service.",
+	Long: `The autonice service receives a message from the poller via zeromq containing
+the chain ID of each relay request. The service boosts the "nice" of the
+corresponding blockchain (if any) and maintains a timer to revert the
+"niceness" after a period of no relays being received. The service must
+be run on all blockchain nodes where this functionality is desired.
 
-There are two parts to this project. A prometheus poller that monitors the
-prometheus output of pocket-core for relay requests. And a client process
-receives messages from the poller when relay requests are received.
-These messages identify which blockchain is the target of the relays
-so that its "niceness" can be boosted.
+On the pocket-core node, the service must run the poller as well via the
+--with-poller flag.
 
-Both parts of the process run in the same process, but the poller is not
-required on blockchain nodes that are not running pocket-core. So for
-blockchain nodes start just the client:
-
-   autonice client
-
-and for nodes that are running pocket-core as well as optionally running
-blockchain nodes start the client and the poller:
-
-   autonice client --withPoller
+Use the --dry-run flag verify communication between poller and blockchain
+servers without changing the nice value of any process.
 `,
+	Run: func(cmd *cobra.Command, args []string) {
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+		viper.Set("dry_run", dryRun)
+		if poller {
+			go prometheusPoller.Start(ctx)
+		}
+		go client.Start(ctx)
+
+		sigs := make(chan os.Signal, 1)
+		signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+		<-sigs
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -47,4 +58,9 @@ func Execute() {
 
 func init() {
 	cobra.OnInitialize(config.InitConfig)
+	clientCmd.Flags().BoolVar(&poller, "with-poller", false,
+		`starts the client and the prometheus poller
+(use on the server that is running pocket-core)`)
+	clientCmd.Flags().BoolVar(&dryRun, "dry-run", false,
+		`runs all functionality except renicing processes`)
 }
