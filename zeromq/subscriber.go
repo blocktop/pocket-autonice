ec0da2092207ca01ab8723aa6c13f1c96d4b1e2b
strobus <22136798+strobus@users.noreply.github.com>
2022-05-01 10:49:17 -0400

    periodically resubscribe

--- zeromq/subscriber.go
+++ zeromq/subscriber.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	log "github.com/sirupsen/logrus"
 	"github.com/zeromq/goczmq"
+	"time"
 )
 
 type Subscriber struct {
@@ -36,11 +37,16 @@ func (s *Subscriber) Start() {
 }
 
 func (s *Subscriber) receiveMessages() {
+	ticker := time.NewTicker(5 * time.Second)
+
 	for {
 		select {
 		case <-s.ctx.Done():
 			log.Debug("exiting message receiver")
 			return
+		case <-ticker.C:
+			// ensure subscription in case publisher stops and restarts
+			s.channeler.Subscribe(s.topic)
 		case data := <-s.channeler.RecvChan:
 			log.Debugf("received %#v", data)
 			if len(data) > 1 {
