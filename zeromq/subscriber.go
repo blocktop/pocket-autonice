a5b41277c99973dd9b82bc573c3cba52eeed89b1
strobus <22136798+strobus@users.noreply.github.com>
2022-05-16 23:14:30 -0400

    switch to pgm

--- zeromq/subscriber.go
+++ zeromq/subscriber.go
@@ -62,7 +62,7 @@ func (s *Subscriber) Start() error {
 	if subBindAddr == "" {
 		endpoint = fmt.Sprintf("tcp://%s", subPubAddr)
 	} else {
-		endpoint = fmt.Sprintf("epgm://%s;%s", subBindAddr, subPubAddr)
+		endpoint = fmt.Sprintf("pgm://%s;%s", subBindAddr, subPubAddr)
 	}
 	if err = sock.Connect(endpoint); err != nil {
 		return errors.Wrap(err, "failed to connect zmq subscriber socket")
